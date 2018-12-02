5883dbc126a80658698d8e0df8eb46d6fecca1a8
Elaine Cadman <elaine.cadman@gmail.com>
2018-12-02 11:06:55 -0800

    still no idea

--- letter.js
+++ letter.js
@@ -1,13 +1,13 @@
 //Letter.js should not require any other files. Write Letter.js first and test it on its 
 //own before moving on
 
-let guesses = [];
+let guesses = ["p"];
 
 // Contains a constructor, Letter. This constructor should be able to either display an 
 //underlying character or a blank placeholder (such as an underscore), depending on 
 //whether or not the user has guessed the letter. 
 //That means the constructor should define:
-function Letter(){
+let Letter = function(){
 // A string value to store the underlying character for the letter
     this.char = "";
 // A boolean value that stores whether that letter has been guessed yet
@@ -15,20 +15,29 @@ function Letter(){
 // A function that returns the underlying character if the letter has been guessed, 
 //or a placeholder (like an underscore) if the letter has not been guessed
 
-function letterGuess(){
-    if (guesses.includes(this.char)){
-        return(this.char);
+this.letterPush = function(entry){
+    if (guesses.includes(entry)){
+        console.log("p?");
+        return(entry);
     }else{
+        console.log("_?");
         return("_");
     }
+    
 }
 // A function that takes a character as an argument and checks it against the underlying 
 //character, updating the stored boolean value to true if it was guessed correctly
-function checkLetter(x) {
+
+this.guess = function(x){
     if(x === this.char){
         this.guessed = true;
+        console.log("true");
     }
 }
 
+// console.log(Letter.letterPush("p"));
+// guess("p");
 }
 
+module.exports = Letter;
+console.log("nothing's working :(")
--- word.js
+++ word.js
@@ -1,18 +1,33 @@
 // Word.js should only require Letter.js Write Letter.js 2nd and test it on its 
 //own before moving on
 
-var Word = require("./letter")
+const Letter = require("./letter")
 
 // Contains a constructor, Word that depends on the Letter constructor. This is used 
 //to create an object representing the current word the user is attempting to guess. 
 //That means the constructor should define:
-
-
+let Word = function(){
 // An array of new Letter objects representing the letters of the underlying word
+let chosenWord = [];
+
+let spaces = new Letter();
+
 // A function that returns a string representing the word. This should call the function 
 //on each letter object (the first function defined in Letter.js) that displays the 
 //character or an underscore and concatenate those together.
+let showWord = [];
 
+function returnWord(){
+    for(y of chosenWord){
+        showWord.push(checkLetter(y));
+    }
+}
 // A function that takes a character as an argument and calls the guess function on each 
 //letter object (the second function defined in Letter.js)
+function guessChar(z){
+    guess();
+}
+}
+
+
 
