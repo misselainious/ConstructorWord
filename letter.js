1b7a7f3986c4e97a7222ed2116476d73d4120ff8
Elaine Cadman <elaine.cadman@gmail.com>
2018-12-04 10:08:58 -0800

    word.js

--- letter.js
+++ letter.js
@@ -1,43 +1,48 @@
 //Letter.js should not require any other files. Write Letter.js first and test it on its 
 //own before moving on
 
-let guesses = ["p"];
 
 // Contains a constructor, Letter. This constructor should be able to either display an 
 //underlying character or a blank placeholder (such as an underscore), depending on 
 //whether or not the user has guessed the letter. 
 //That means the constructor should define:
-let Letter = function(){
+let Letter = function(char){
 // A string value to store the underlying character for the letter
-    this.char = "";
+    this.visible = !/[a-z1-9]/i.test(char);
+    this.char = char;
 // A boolean value that stores whether that letter has been guessed yet
     this.guessed = false;
 // A function that returns the underlying character if the letter has been guessed, 
 //or a placeholder (like an underscore) if the letter has not been guessed
-
-this.letterPush = function(entry){
-    if (guesses.includes(entry)){
-        console.log("p?");
-        return(entry);
-    }else{
-        console.log("_?");
-        return("_");
-    }
-    
 }
+
+Letter.prototype.toString = function() {
+    if (this.visible === true) {
+      return this.char;
+    }
+    return "_";
+  };
+// this.letterPush = function(entry){
+//     if (guesses.includes(entry)){
+//         console.log("p?");
+//         return(entry);
+//     }else{
+//         console.log("_?");
+//         return("_");
+//     }
+// }
 // A function that takes a character as an argument and checks it against the underlying 
 //character, updating the stored boolean value to true if it was guessed correctly
 
-this.guess = function(x){
-    if(x === this.char){
+Letter.prototype.guess = function(userGuess){
+    if (userGuess.toUpperCase() === this.char.toUpperCase()) {
         this.guessed = true;
-        console.log("true");
-    }
-}
+        return true;
+      }
+    
+      return false;
+    };
 
-// console.log(Letter.letterPush("p"));
-// guess("p");
-}
+
 
 module.exports = Letter;
-console.log("nothing's working :(")
--- word.js
+++ word.js
@@ -3,12 +3,32 @@
 
 const Letter = require("./letter")
 
+var wordBank = ['purple', 'red', 'yellow', 'green'];
+
 // Contains a constructor, Word that depends on the Letter constructor. This is used 
 //to create an object representing the current word the user is attempting to guess. 
 //That means the constructor should define:
-let Word = function(){
+function Word(word){
 // An array of new Letter objects representing the letters of the underlying word
-let chosenWord = [];
+this.word = word;
+this.letter = [];
+this.allCorrect = false;
+
+this.randomWord = function(){
+    let randomNumber = Math.floor(Math.random() * wordBank.length);
+    this.currentWord = wordBank[randomNumber];
+    this.lettersLength = currentWord.length;
+    return this.currentWord;
+  }
+  this.splitWord = function() {
+    this.lettersArray = this.split(""); 
+    return this.lettersArray;
+  }
+  this.getLetters = function () {
+    for (var i = 0; i < this.length; i++) {
+      var newLetter = new Letter(this.word[i]);
+      this.letters.push(newLetter);
+    };
 
 let spaces = new Letter();
 
@@ -29,5 +49,42 @@ function guessChar(z){
 }
 }
 
+//******************************* */
 
 
+  //checks to see if the letter is in the word
+  this.wordCheck = function () {
+    if (this.letters.every(function (ltr) {
+      return ltr.appear === true;
+    })) {
+      this.wordFound = true;
+      return true;
+    };
+  };
+
+
+  this.checkLetter = function (guessedLetter) {
+    let whatToReturn = true;
+    this.letters.forEach(function (ltr) {
+      if (ltr.letter === guessedLetter) {
+        ltr.appear = true;
+        whatToReturn++;
+      };
+    });
+    return whatToReturn;
+  };
+
+
+  this.wordShow = function () {
+    let display = '';
+    this.letters.forEach(function (ltr) {
+      let currentLetter = ltr.letterShow();
+      display += currentLetter;
+    });
+    return display;
+  };
+};
+
+
+
+module.exports = Word;
